daa7e101cb78a66150b24e9c855b9493f6d26559
b07902040 <b07902040@ntu.edu.tw>
2021-06-15 11:59:56 +0800

    hack4

--- hack4/backend/src/resolvers/Mutation.js
+++ hack4/backend/src/resolvers/Mutation.js
@@ -3,25 +3,30 @@ const Mutation = {
     if ( db.people === null ) {
       return false;
     }
-    for ( let i = 0; i < args.data.length; i++ ) {
+    for ( i = 0; i < args.data.length; i++ ) {
       var fil = await db.people.filter((val, key) => val.ssn === args.data[i].ssn);
-      if ( fil.length === 0 || fil === null || fil === [] ) {
+      if ( fil >= 1 ) {
         var loc = ({ 
           name: args.data[i].location.name, 
           description: args.data[i].location.description
         });
-        var newPerson = ({
-          ssn: args.data[i].ssn,
-          name: args.data[i].name,
-          location: loc,
-          severity: args.data[i].severity,
-        })
-        await db.people.push(newPerson);
-      }
-      else {
-        
+        db.people[i].name = args.data[i].location.name;
+        db.people[i].location = loc;
+        db.people[i].severity = args.data[i].severity;
+        return true;
       }
     }
+    var loc = ({ 
+      name: args.data[i].location.name, 
+      description: args.data[i].location.description
+    });
+    var newPerson = ({
+      ssn: args.data[i].ssn,
+      name: args.data[i].name,
+      location: loc,
+      severity: args.data[i].severity,
+    })
+    await db.people.push(newPerson);
     return true;
   }
 };
