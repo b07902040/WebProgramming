dc3a2b052e3ef40af6ed5d4469c1ddd262eaf5f3
b07902040 <b07902040@ntu.edu.tw>
2021-06-18 21:36:03 +0800

    fix-Mutation.js

--- hack4/backend/src/resolvers/Mutation.js
+++ hack4/backend/src/resolvers/Mutation.js
@@ -3,30 +3,14 @@ const Mutation = {
     if ( db.people === null ) {
       return false;
     }
-    for ( i = 0; i < args.data.length; i++ ) {
-      var fil = await db.people.filter((val, key) => val.ssn === args.data[i].ssn);
-      if ( fil >= 1 ) {
-        var loc = ({ 
-          name: args.data[i].location.name, 
-          description: args.data[i].location.description
-        });
-        db.people[i].name = args.data[i].location.name;
-        db.people[i].location = loc;
-        db.people[i].severity = args.data[i].severity;
-        return true;
+    args.data.forEach((per) => {
+      const fil = db.people.findIndex((ssn) => ssn.ssn === per.ssn);
+      if ( fil !== -1 ) {
+        db.people[fil] = per;
       }
-    }
-    var loc = ({ 
-      name: args.data[i].location.name, 
-      description: args.data[i].location.description
+      else db.people.push(per);
     });
-    var newPerson = ({
-      ssn: args.data[i].ssn,
-      name: args.data[i].name,
-      location: loc,
-      severity: args.data[i].severity,
-    })
-    await db.people.push(newPerson);
+    
     return true;
   }
 };
